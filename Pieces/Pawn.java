91e3bc9c22ec3c27bf1ca81b21034f91dbcf140c
finn <f.krempien@campus.tu-berlin.de>
2020-07-04 11:35:32 +0200

    Added a toString (for printBoard od board) fixed view moves to always generate an new list fixed view moves to use the copyConstructor of Position when adding stuff to possible moves

--- Pieces/Pawn.java
+++ Pieces/Pawn.java
@@ -2,40 +2,42 @@ public class Pawn extends Piece {
 
     Position startPosition;
 
-    public Pawn(Owner owner, Position pos, Board board) {
-        super(owner, pos, board);
-        this.startPosition = pos;
+    public Pawn(Owner owner, Position position, Board board) {
+        super(owner, position, board);
+        this.startPosition = position;
     }
 
     @Override
     public void viewMoves() {
-
-        Position tmp = new Position(this.pos);
+        this.possibleMoves.clear();
+        Position tmp = new Position(this.position);
 
         //Piece moves up or down depending if it's Black(down) or White(up)
         int BW = 1;
         if (this.owner == Owner.White) BW = -1;
-
-        //checks the three moves that might always work
-        tmp.setyPos(this.pos.getyPos() + BW);
+        //checks the three moves that always might work
+        tmp.setyPos(this.position.getyPos() + BW);
         if (tmp.inRange() && this.board.getPiece(tmp) == null) {
-            this.possibleMoves.add(tmp);
+            this.possibleMoves.add(new Position(tmp));
         }
-        tmp.setxPos(this.pos.getxPos() - 1);
+        tmp.setxPos(this.position.getxPos() - 1);
         if (tmp.inRange() && tmp.getxPos() >= 0 && this.board.getPiece(tmp) != null && board.getPiece(tmp).owner != this.owner) {
-            this.possibleMoves.add(tmp);
+            this.possibleMoves.add(new Position(tmp));
         }
-        tmp.setxPos(this.pos.getxPos() + 1);
+        tmp.setxPos(this.position.getxPos() + 1);
         if (tmp.inRange() && board.getPiece(tmp) != null && board.getPiece(tmp).owner != this.owner) {
-            this.possibleMoves.add(tmp);
+            this.possibleMoves.add(new Position(tmp));
         }
-
         //checks the move that's only possible ass the first one
-        if (tmp.getxPos() >= 0 && this.pos == this.startPosition) {
-            tmp.setyPos(this.pos.getyPos() + (2 * BW));
-            if (tmp.inRange() && board.getPiece(tmp) == null) {
-                this.possibleMoves.add(tmp);
-            }
+        tmp.setyPos(this.position.getyPos() + (2 * BW));
+        tmp.setxPos(this.position.getxPos());
+        if (this.position.equals(this.startPosition) && tmp.inRange() && board.getPiece(tmp) == null) {
+            this.possibleMoves.add(new Position(tmp));
         }
+    }
+
+    @Override
+    public String toString() {
+        return "P";
     }
 }
