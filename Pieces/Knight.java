2873504943e7fd25c2159ba9176e854f3c99c2e2
finn <f.krempien@campus.tu-berlin.de>
2020-07-04 11:34:42 +0200

    Added a toString (for printBoard od board) fixed view moves to always generate an new list

--- Pieces/Knight.java
+++ Pieces/Knight.java
@@ -1,28 +1,35 @@
 import java.util.LinkedList;
 
 public class Knight extends Piece {
-    public Knight(Owner owner, Position pos, Board board) {
-        super(owner, pos, board);
+
+    public Knight(Owner owner, Position position, Board board) {
+        super(owner, position, board);
     }
 
     @Override
     public void viewMoves() {
         //creates a list of all positions to check
+        this.possibleMoves.clear();
         LinkedList<Position> tmpPositions = new LinkedList<>();
-        tmpPositions.add(new Position(this.pos.getxPos() + 2, this.pos.getyPos() + 1));
-        tmpPositions.add(new Position(this.pos.getxPos() + 2, this.pos.getyPos() - 1));
-        tmpPositions.add(new Position(this.pos.getxPos() - 2, this.pos.getyPos() + 1));
-        tmpPositions.add(new Position(this.pos.getxPos() - 2, this.pos.getyPos() - 1));
-        tmpPositions.add(new Position(this.pos.getxPos() + 1, this.pos.getyPos() + 2));
-        tmpPositions.add(new Position(this.pos.getxPos() + 1, this.pos.getyPos() - 1));
-        tmpPositions.add(new Position(this.pos.getxPos() - 1, this.pos.getyPos() + 1));
-        tmpPositions.add(new Position(this.pos.getxPos() - 1, this.pos.getyPos() - 1));
+        tmpPositions.add(new Position(this.position.getxPos() + 2, this.position.getyPos() + 1)); //two up one right
+        tmpPositions.add(new Position(this.position.getxPos() + 2, this.position.getyPos() - 1)); //two up one left
+        tmpPositions.add(new Position(this.position.getxPos() - 2, this.position.getyPos() + 1)); //two down one right
+        tmpPositions.add(new Position(this.position.getxPos() - 2, this.position.getyPos() - 1)); //two down one left
+        tmpPositions.add(new Position(this.position.getxPos() + 1, this.position.getyPos() + 2)); //one up two right
+        tmpPositions.add(new Position(this.position.getxPos() + 1, this.position.getyPos() - 2)); //one up two left
+        tmpPositions.add(new Position(this.position.getxPos() - 1, this.position.getyPos() + 2)); //one down two right
+        tmpPositions.add(new Position(this.position.getxPos() - 1, this.position.getyPos() - 2)); //one down two left
 
         //checks all of them
-        for(Position position : tmpPositions){
-            if(position.inRange() && (board.getPiece(position) == null || board.getPiece(position).owner != this.owner)){
+        for (Position position : tmpPositions) {
+            if (position.inRange() && (board.getPiece(position) == null || board.getPiece(position).owner != this.owner)) {
                 this.possibleMoves.add(position);
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        return "K";
     }
 }
