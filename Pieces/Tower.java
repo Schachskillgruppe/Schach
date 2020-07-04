b2df8708678b54c8e9972ae024380a33303eaa0a
finn <f.krempien@campus.tu-berlin.de>
2020-07-04 11:36:20 +0200

    Added a toString (for printBoard od board) fixed view moves to always generate an new list

--- Pieces/Tower.java
+++ Pieces/Tower.java
@@ -1,34 +1,40 @@
 public class Tower extends Piece {
 
-    public Tower(Owner owner, Position pos, Board board) {
-        super(owner, pos, board);
+    public Tower(Owner owner, Position position, Board board) {
+        super(owner, position, board);
     }
 
     @Override
     public void viewMoves() {
         //sets a few needed temporary variables
+        this.possibleMoves.clear();
         boolean left = true;
         boolean right = true;
         boolean up = true;
         boolean down = true;
-        Position tmpL = new Position(this.pos);
-        Position tmpR = new Position(this.pos);
-        Position tmpU = new Position(this.pos);
-        Position tmpD = new Position(this.pos);
+        Position tmpL = new Position(this.position);
+        Position tmpR = new Position(this.position);
+        Position tmpU = new Position(this.position);
+        Position tmpD = new Position(this.position);
 
         //checks all 4 directions
-        for (int i = 0; i <= 7; i++) {
-            tmpL.setxPos(this.pos.getxPos() - i);
-            tmpR.setxPos(this.pos.getxPos() + i);
-            tmpU.setyPos(this.pos.getyPos() + i);
-            tmpD.setyPos(this.pos.getyPos() - i);
+        for (int i = 1; i <= 7; i++) {
+            tmpL.setxPos(this.position.getxPos() - i);
+            tmpR.setxPos(this.position.getxPos() + i);
+            tmpU.setyPos(this.position.getyPos() + i);
+            tmpD.setyPos(this.position.getyPos() - i);
 
             left = canWalk(left, tmpL);
             right = canWalk(right, tmpR);
             up = canWalk(up, tmpU);
             down = canWalk(down, tmpD);
 
-            if(!(left||right||up||down)) return;
+            if (!(left || right || up || down)) return;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "T";
     }
 }
