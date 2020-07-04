7be4e9c3d5e951c13ceb91fa3b95ed81c163ab89
finn <f.krempien@campus.tu-berlin.de>
2020-07-04 11:36:09 +0200

    Added a toString (for printBoard od board) fixed view moves to always generate an new list

--- Pieces/Queen.java
+++ Pieces/Queen.java
@@ -1,10 +1,12 @@
 public class Queen extends Piece {
-    public Queen(Owner owner, Position pos, Board board) {
-        super(owner, pos, board);
+
+    public Queen(Owner owner, Position position, Board board) {
+        super(owner, position, board);
     }
 
     @Override
     public void viewMoves() {
+        this.possibleMoves.clear();
         //sets a few needed temporary variables
         boolean topleft = true;
         boolean topright = true;
@@ -14,26 +16,29 @@ public class Queen extends Piece {
         boolean right = true;
         boolean up = true;
         boolean down = true;
-        Position tmpTL = new Position(this.pos);
-        Position tmpTR = new Position(this.pos);
-        Position tmpBL = new Position(this.pos);
-        Position tmpBR = new Position(this.pos);
-        Position tmpL = new Position(this.pos);
-        Position tmpR = new Position(this.pos);
-        Position tmpU = new Position(this.pos);
-        Position tmpD = new Position(this.pos);
-
+        Position tmpTL = new Position(this.position);
+        Position tmpTR = new Position(this.position);
+        Position tmpBL = new Position(this.position);
+        Position tmpBR = new Position(this.position);
+        Position tmpL = new Position(this.position);
+        Position tmpR = new Position(this.position);
+        Position tmpU = new Position(this.position);
+        Position tmpD = new Position(this.position);
 
         //checks all 8 directions
-        for (int i = 0; i <= 7; i++) {
-            tmpTL.setxPos(this.pos.getxPos() - i); tmpTL.setyPos(this.pos.getyPos() + i);
-            tmpTR.setxPos(this.pos.getxPos() + i); tmpTR.setyPos(this.pos.getyPos() + i);
-            tmpBL.setxPos(this.pos.getxPos() - i); tmpBL.setyPos(this.pos.getyPos() - i);
-            tmpBR.setxPos(this.pos.getxPos() + i); tmpBR.setyPos(this.pos.getyPos() - i);
-            tmpL.setxPos(this.pos.getxPos() - i);
-            tmpR.setxPos(this.pos.getxPos() + i);
-            tmpU.setyPos(this.pos.getyPos() + i);
-            tmpD.setyPos(this.pos.getyPos() - i);
+        for (int i = 1; i <= 7; i++) {
+            tmpTL.setxPos(this.position.getxPos() - i);
+            tmpTL.setyPos(this.position.getyPos() + i);
+            tmpTR.setxPos(this.position.getxPos() + i);
+            tmpTR.setyPos(this.position.getyPos() + i);
+            tmpBL.setxPos(this.position.getxPos() - i);
+            tmpBL.setyPos(this.position.getyPos() - i);
+            tmpBR.setxPos(this.position.getxPos() + i);
+            tmpBR.setyPos(this.position.getyPos() - i);
+            tmpL.setxPos(this.position.getxPos() - i);
+            tmpR.setxPos(this.position.getxPos() + i);
+            tmpU.setyPos(this.position.getyPos() + i);
+            tmpD.setyPos(this.position.getyPos() - i);
 
             topleft = canWalk(topleft, tmpTL);
             topright = canWalk(topright, tmpTR);
@@ -44,8 +49,12 @@ public class Queen extends Piece {
             up = canWalk(up, tmpU);
             down = canWalk(down, tmpD);
 
-            if(!(topleft||topright||bottomleft||bottomright||left||right||up||down)) return;
+            if (!(topleft || topright || bottomleft || bottomright || left || right || up || down)) return;
         }
+    }
 
+    @Override
+    public String toString() {
+        return "Q";
     }
 }
