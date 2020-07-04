8230649c3fc3987d68a60969ae4416148ae053e3
finn <f.krempien@campus.tu-berlin.de>
2020-07-04 11:36:15 +0200

    Added a toString (for printBoard od board) fixed view moves to always generate an new list

--- Pieces/Rook.java
+++ Pieces/Rook.java
@@ -1,35 +1,44 @@
-public class Rook extends Piece{
+public class Rook extends Piece {
 
-    public Rook(Owner owner, Position pos, Board board) {
-        super(owner, pos, board);
+    public Rook(Owner owner, Position position, Board board) {
+        super(owner, position, board);
     }
 
     @Override
     public void viewMoves() {
+        this.possibleMoves.clear();
         //sets a few needed temporary variables
         boolean topleft = true;
         boolean topright = true;
         boolean bottomleft = true;
         boolean bottomright = true;
-        Position tmpTL = new Position(this.pos);
-        Position tmpTR = new Position(this.pos);
-        Position tmpBL = new Position(this.pos);
-        Position tmpBR = new Position(this.pos);
+        Position tmpTL = new Position(this.position);
+        Position tmpTR = new Position(this.position);
+        Position tmpBL = new Position(this.position);
+        Position tmpBR = new Position(this.position);
 
         //checks all 4 directions
-        for (int i = 0; i <= 7; i++) {
-            tmpTL.setxPos(this.pos.getxPos() - i); tmpTL.setyPos(this.pos.getyPos() + i);
-            tmpTR.setxPos(this.pos.getxPos() + i); tmpTR.setyPos(this.pos.getyPos() + i);
-            tmpBL.setxPos(this.pos.getxPos() - i); tmpBL.setyPos(this.pos.getyPos() - i);
-            tmpBR.setxPos(this.pos.getxPos() + i); tmpBR.setyPos(this.pos.getyPos() - i);
+        for (int i = 1; i <= 7; i++) {
+            tmpTL.setxPos(this.position.getxPos() - i);
+            tmpTL.setyPos(this.position.getyPos() + i);
+            tmpTR.setxPos(this.position.getxPos() + i);
+            tmpTR.setyPos(this.position.getyPos() + i);
+            tmpBL.setxPos(this.position.getxPos() - i);
+            tmpBL.setyPos(this.position.getyPos() - i);
+            tmpBR.setxPos(this.position.getxPos() + i);
+            tmpBR.setyPos(this.position.getyPos() - i);
 
             topleft = canWalk(topleft, tmpTL);
             topright = canWalk(topright, tmpTR);
             bottomleft = canWalk(bottomleft, tmpBL);
             bottomright = canWalk(bottomright, tmpBR);
 
-            if(!(topleft||topright||bottomleft||bottomright)) return;
+            if (!(topleft || topright || bottomleft || bottomright)) return;
         }
+    }
 
+    @Override
+    public String toString() {
+        return "R";
     }
 }
